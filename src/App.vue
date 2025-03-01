d61be677aab2c8f3e433567a9432db07af75f7c3
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-03-01 15:55:11 +0500

    started

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <AppInfo />
+      <SearchPanel />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInfo from './Components/AppInfo.vue'
+import SearchPanel from './Components/SearchPanel.vue'
+export default {
+  components: {
+    AppInfo,
+    SearchPanel
+  }
+}
+</script>
+<style></style>
